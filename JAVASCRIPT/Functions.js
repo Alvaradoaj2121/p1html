b19e8e52ae828e9809c823696b82a10cb1ccbf56
Alvaradoaj2121 <arezlyj21@gmail.com>
2024-02-15 11:56:07 -0600

    se agrega varios tipos de input

--- JAVASCRIPT/Functions.js
+++ JAVASCRIPT/Functions.js
@@ -1,3 +1,8 @@
 funcion changeColor(){
   document.getElementById("palabrita").style.backgroundColor="green";
+document.getElementById("palabrita").textContent="hola";
+}
+
+function enviarMsg(){
+  alert("Hola amigo")
 }
